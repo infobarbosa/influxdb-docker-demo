1eba34c676e182ade9982db58001c565ac1b6e11
infobarbosa <infobarbosa@yahoo.com.br>
2024-07-17 12:15:56 -0300

    chore: Update InfluxDB bucket name in pedidos.sh script

--- assets/scripts/pedidos.sh
+++ assets/scripts/pedidos.sh
@@ -4,7 +4,7 @@ while(true)
 do
     if [ $(expr $i % 2 ) != "0" ]; then
         curl --request POST \
-            "http://localhost:8086/api/v2/write?org=infobarbosa&bucket=default&precision=ns" \
+            "http://localhost:8086/api/v2/write?org=infobarbosa&bucket=ecommerce&precision=ns" \
             --header "Authorization: Token 3y1c3NnlmA1kA061YlROSO0gE5a1ppH_1Ig5HSMCsCX3VKF6zkrBwAtC-Hr6c_TTU8B9kwYOPphDq6hwyw5tLw==" \
             --header "Content-Type: text/plain; charset=utf-8" \
             --header "Accept: application/json" \
@@ -13,7 +13,7 @@ do
     
     else
         curl --request POST \
-            "http://localhost:8086/api/v2/write?org=infobarbosa&bucket=default&precision=ns" \
+            "http://localhost:8086/api/v2/write?org=infobarbosa&bucket=ecommerce&precision=ns" \
             --header "Authorization: Token 3y1c3NnlmA1kA061YlROSO0gE5a1ppH_1Ig5HSMCsCX3VKF6zkrBwAtC-Hr6c_TTU8B9kwYOPphDq6hwyw5tLw==" \
             --header "Content-Type: text/plain; charset=utf-8" \
             --header "Accept: application/json" \
