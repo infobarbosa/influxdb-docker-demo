cea29d3dd41ba6cdbdecdbbf7f33d391d9dcb8ef
infobarbosa <infobarbosa@yahoo.com.br>
2024-07-21 14:49:49 -0300

    chore: Add cloud9.sh script for setting up Cloud9 environment

---
+++ assets/scripts/cloud9.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+echo "### Atualizando o sistema ###"
+sudo apt update -y
+
+echo "### Instalando o pacote boto3  ###"
+pip install boto3
+
+echo "### Instalando o jq, a lightweight and flexible command-line JSON processor  ###"
+sudo apt install -y jq
+
+echo "### O ID da instância EC2 do ambiente Cloud9 ###"
+export CLOUD9_EC2_INSTANCE_ID=$(curl http://169.254.169.254/latest/meta-data//instance-id)
+
+export CLOUD9_EC2_PUBLIC_DNS=$(aws ec2 describe-instances --instance-id $CLOUD9_EC2_INSTANCE_ID | jq -r .Reservations[0].Instances[0].NetworkInterfaces[0].Association.PublicDnsName)
+
+export CLOUD9_EC2_SECURITY_GROUP_ID=$(aws ec2 describe-instances --instance-id $CLOUD9_EC2_INSTANCE_ID | jq -r .Reservations[0].Instances[0].NetworkInterfaces[0].Groups[0].GroupId)
+
+aws ec2 authorize-security-group-ingress --group-id $CLOUD9_EC2_SECURITY_GROUP_ID --protocol tcp --port 8086 --cidr 0.0.0.0/0
